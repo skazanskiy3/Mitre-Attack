3ab25825b2e1058f7ab7e3426d060119d63bebaa
Stan Kazanskiy <Stan.Kazanskiy@Honeywell.com>
2020-07-26 13:13:15 -0400

    update read me, implement api

---
+++ src/main/java/com/demo/mitreattack/MitreAttackApplication.java
@@ -0,0 +1,13 @@
+package com.demo.mitreattack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MitreAttackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MitreAttackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/mitreattack/MitreAttackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.mitreattack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MitreAttackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
